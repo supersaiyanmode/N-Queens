dd7daf6748c2564fa039138339082f4b44148663
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2016-09-08 22:27:48 -0400

    Code clean up..

--- main.py
+++ main.py
@@ -1,46 +1,31 @@
 import sys
 
-def inside(r, c, n):
-    return (0 <= r < n) and (0 <= c < n)
-
 def can_place(board, n, (row, col)):
-    rng = range(1, row+1)
+    rng = range(2, row+1) #No need to check the immediate row above. 
     if any(board[row - i][col - i] for i in rng if 0 <= row-i < n and 0 <= col-i < n):
         return False
     if any(board[row - i][col + i] for i in rng if 0 <= row-i < n and 0 <= col+i < n):
         return False
-    if any(board[row - i][col] for i in rng):
-        return False
+    #No need to check columns, just diagonals is fine.
     return True
 
-def place_queens(board, n=None, row=0, good_columns=None, bad_columns=None):
-    if n is None:
-        n = len(board)
-    if good_columns is None:
-        good_columns = set(range(n))
-    if bad_columns is None:
-        bad_columns = set()
-
-    all_cols = set(range(n))
-
+def place_queens(board, n, all_cols, row, good_columns, bad_columns):
     for col in good_columns - bad_columns:
         if can_place(board, n, (row, col)):
             board[row][col] = 1
             if row == n - 1:
                 return True
-            bad_cols = {col + x for x in (-1, 0, 1) if 0 <= col + x < n} | bad_columns
+            bad_cols = {x for x in (col - 1, col, col + 1) if 0 <= x < n} | bad_columns
             good_cols = all_cols - bad_cols
-            if place_queens(board, n, row + 1, good_cols, bad_columns | {col}):
+            if place_queens(board, n, all_cols, row + 1, good_cols, bad_columns | {col}):
                 return True
             board[row][col] = 0
     return False
 
-
 def main():
     n = int(sys.argv[1])
     board = [[0]*n for _ in range(n)]
-    if place_queens(board):
-        print "--"
+    if place_queens(board, n, set(range(n)), 0, set(range(n)), set()):
         print "\n".join(str(r) for r in board)
     else:
         print "Can't place.."
